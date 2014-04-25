fdba5d58e2f3fb76351d191cdaebd1ec983b97df
Grant Van Sant <grantwenzlau@gmail.com>
2014-04-25 15:36:22 -0700

    minor style

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "https://s3-us-west-2.amazonaws.com/slyfoxco/slyfox-avatar.png"
   
-validates_attachment_content_type :avatar, :content_type => /\Aimage/
-# Validate filename
-#validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
-# Explicitly do not validate
-#do_not_validate_attachment_file_type :avatar
+  validates_attachment_content_type :avatar, :content_type => /\Aimage/
 
   has_many :posts, dependent: :destroy
 
