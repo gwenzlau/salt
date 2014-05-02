fd4101e7dc63f866ed63025bdedefdf849f2656a
Grant Van Sant <grantwenzlau@gmail.com>
2014-05-01 21:42:54 -0700

    cookie fix

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,3 +10,4 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 Salt::Application.config.secret_key_base = ENV['RAILS_COOKIE_SECRET']
+Salt::Application.config.secret_key_token = ENV['RAILS_COOKIE_SECRET']
